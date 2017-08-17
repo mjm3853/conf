276f712c65e8b7a1eb7e99fa4b40aaafd47372a4
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-08-17 08:38:34 -0400

    start sarch js

---
+++ static/js/module/search.js
@@ -0,0 +1,22 @@
+var events = [
+    {
+        "uri": "/dashboard/stats",
+        "content": "---\ntitle: \"Tags\"\ndate: 2017-07-03T15:21:37-05:00\nslug: /tags\n---",
+        "tags": []
+    },
+    {
+        "uri": "/post/music/2017/09/01/electric_zoo",
+        "content": "---\ntitle: \"Electric Zoo\"\ndate: 2017-09-01T00:03:14-04:00\nend: 2017-09-03T00:03:14-04:00\ntags: [\"EastCoast\", \"Fall\", \"EDM\"]\ncategories: [\"Music\"]\ndraft: false\nsubtitle: \"This year’s theme—The 6th Boro—is both an homage to the greatest city in the world and a brand new neighborhood unto itself\"\nlocation: \"Randall's Island Park, New York, New York\"\nlink: \"http://electriczoofestival.com/\"\nearlyCost: \"$XXX.XX\"\nnormalCost: \"$XXX.XX\"\n---\n\n!--more--\n\nHeadliners\n\nDJ Snake\nAbove & Beyond\nGalantis\nDeadmau5\nEric Prydz\nArmin Van Buuren\nZedd\n\n Additional Information\n\nEstablished in 2009 by Made Event, the internationally renowned Electric Zoo Festival is one of New York City’s largest music festivals and features the top names in electronic music, bringing a wide variety of acts from around the world and across the spectrum of electronic music’s various sub-genres. International editions of Electric Zoo have taken place in Mexico City, Tokyo and Shanghai, with Electric Zoo: São Paulo scheduled for  April and further expansion underway. This year’s 9th annual New York edition, themed ‘The 6th Boro’ will take place on September 1-3 at Randall’s Island Park.\n",
+        "tags": []
+    },
+    {
+        "uri": "/post/music/2017/09/02/made_in_america",
+        "content": "---\ntitle: \"Made in America\"\ndate: 2017-09-02T23:21:38-04:00\nend: 2017-09-03T23:21:38-04:00\ntags: [\"Rap\", \"EDM\", \"R&B\", \"EastCoast\"]\ncategories: [\"Music\"]\ndraft: false\nsubtitle: \"Brought to you by Budweiser\"\nlocation: \"Ben Franklin Parkway, Philadelphia, Pennsylvania\"\nlink: \"http://www.madeinamericafest.com/7/#/home\"\nearlyCost: \"$XXX.XX\"\nnormalCost: \"$XXX.XX\"\n---\n\n!--more--\n\nHeadliners\n\nJay Z\nJ. Cole\nThe Chainsmokers\nKaskade\nMarshmello\nMigos\nSolange\n",
+        "tags": []
+    },
+    {
+        "uri": "/post/music/2017/10/06/2017_acl_wk1",
+        "content": "---\ntitle: \"Austin City Limits (Week 1)\"\ndate: 2017-10-06T13:12:56-04:00\nend: 2017-10-08T13:12:56-04:00\ntags: [\"DownSouth\", \"Fall\", \"AllGenres\", \"Food\"]\ncategories: [\"Music\"]\ndraft: false\nsubtitle: \"They call us the live music capital of the world.\"\nlocation: \"Zilker Park, Austin, Texas\"\nlink: \"https://www.aclfestival.com/\"\nearlyCost: \"$XXX.XX\"\nnormalCost: \"$XXX.XX\"\n---\n\n!--more--\n\nHeadliners\n\nJay-Z\nMartin Garrix\nThe XX\nRed Hot Chili Peppers\nChance the Rapper\nIce Cube\nThe Killers\nGorillaz\nRun the Jewels",
+        "tags": []
+    }]
+
